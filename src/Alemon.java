4e9d38fccf63bec1aaaf8a201ba9596adb57125a
Djefferson Schneider Tasarz <7djefferson@gmail.com>
2026-03-09 14:58:47 -0300

    Adiciona classe Alemon com Hallo Welt

---
+++ src/Alemon.java
@@ -0,0 +1,10 @@
+// src/Alemon.java
+public class Alemon {
+    public static void main(String[] args) {
+        halloWelt();
+    }
+    
+    public static void halloWelt() {
+        System.out.println("Hallo Welt!");
+    }
+}
