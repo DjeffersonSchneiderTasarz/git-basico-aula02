c504c3763c76e73ab209dac3e9edff6de8b0dd1e
Djefferson Schneider Tasarz <7djefferson@gmail.com>
2026-03-09 14:52:26 -0300

    Adiciona Hello.java com Hello World

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+// src/Hello.java
+public class Hello {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+    
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+}
